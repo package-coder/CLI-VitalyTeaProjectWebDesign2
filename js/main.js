72d4a22316dc589ab24ecb40fc213d6b4d0a151a
Chris Paul <79316343+package-coder@users.noreply.github.com>
2021-07-22 02:51:45 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -4,23 +4,7 @@ const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) => {
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
-  })
-  item.addEventListener("focusout", function () {
-    this.classList.toggle("collapsible--expanded");
+    
   })
 }
 );
-
-
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("scroll-sticky-id");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("scroll-sticky");
-  } else {
-    header.classList.remove("scroll-sticky");
-  }
-}
